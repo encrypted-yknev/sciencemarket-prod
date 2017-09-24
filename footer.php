884757c49741e18e719af35ef0343973c46b655e
N Venkateshan <venkycse93@gmail.com>
2017-09-24 11:48:06 +0530

    Changed footer style and text

--- footer.php
+++ footer.php
@@ -1,2 +1,8 @@
 
-<div id="footer"><p id="footText">--- &copyBioForum 2017 ---</p></div>
+<div id="footer">
+	</br>
+	<p id="footText"><strong> &copyScience Market</strong> 2017 </br>
+	<em>"If you do not ask the right questions, you do not get the right answers. A question asked in the right way often points to its own answer.” – Edward Hodnett</em>
+	</p>
+	
+</div>
