21679cb43c74d380ba5d72b000619075128f7bf4
N Venkateshan <venkycse93@gmail.com>
2017-09-24 11:49:37 +0530

    Assigned value to option tags

--- get_sub_topics.php
+++ get_sub_topics.php
@@ -6,19 +6,20 @@ if(!$_SESSION["logged_in"])	{
 }
 include "connectDb.php";
 
-$topic_name=htmlspecialchars(stripslashes(trim($_REQUEST['topic'])));
-if(strlen($topic_name)!=0)	{
+$topic_id=htmlspecialchars(stripslashes(trim($_REQUEST['topic'])));
+if(!empty($topic_id))	{
 	try	{
-		$sql_fetch_sub_topics="select a.topic_desc from topics a
+		$sql_fetch_sub_topics="select a.topic_id,a.topic_desc from topics a
 							   inner join topics b 
 							   on a.parent_topic=b.topic_id
-							   where b.topic_desc='".$topic_name."'";
+							   where b.topic_id=".$topic_id;
+		echo '<option value="" selected>--- Select sub-topics ---</option>';
 		foreach($conn->query($sql_fetch_sub_topics) as $result_topics)	{
-			echo '<option>'.$result_topics["topic_desc"].'</option>';
+			echo '<option value="'.$result_topics['topic_id'].'">'.$result_topics['topic_desc'].'</option>';
 		}
 	}
 	catch(PDOException $e)	{
-		echo $e->getMessage();
+		
 	}
 }
 ?>
