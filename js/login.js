56795756e03143cb0aba083908adaedaac62e704
N Venkateshan <venkycse93@gmail.com>
2017-10-02 12:26:22 -0400

    Added timezone set ajax call to set_timezone script

--- js/login.js
+++ js/login.js
@@ -1,4 +1,13 @@
 $(document).ready(function()	{
- $("#err1").css("background-color","#F9F7F7");
+	var tz_offset = (new Date()).getTimezoneOffset();
+	tz_offset = (tz_offset == 0) ? 0: -tz_offset;
+	$.ajax({
+		type:"post",
+		url:"set_timezone.php",
+		data:
+		{
+			"offset":tz_offset
+		}
+	});
+});
 
- });
