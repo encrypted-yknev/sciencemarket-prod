630faf5948a5fca08edd62e90c5a918e35526885
N Venkateshan <venkycse93@gmail.com>
2018-02-04 12:09:21 +0530

    Added notification related functions and fixed few bugs

--- js/header.js
+++ js/header.js
@@ -1,24 +1,34 @@
-/* $(document).on("click","#user-icon-section",function()	{
-	$("#profile-section").show();
-});
-
-$(document).on("click","#more-link",function()	{
-	$("#head-nav-menu").show();
-});  */
-
 $(document).ready(function()	{
-	$("body").click(function()	{
-		$("#profile-section").hide();
-		$("#head-nav-menu").hide();
-		$("#srch-result").hide();
-		$("#srch-result-mobile").hide();
+	var tz_offset = (new Date()).getTimezoneOffset();
+	tz_offset = (tz_offset == 0) ? 0: -tz_offset;
+	$.ajax({
+		type:"post",
+		url:"../set_timezone.php",
+		data:
+		{
+			"offset":tz_offset
+		}
 	});
-	$(document).on("click","#user-icon-section",function()	{
-		$("#profile-section").show();
+	refreshNotify();
+	$("body").click(function()	{
+		$("#head-more-menu").fadeOut(100);
+		$("#head-pro-section").fadeOut(100);
+		$("#notification-section").fadeOut(100);
+		$("#srch-result").fadeOut(100);
+		$("#srch-result-mobile").fadeOut(100);
 	});
 
+	$(document).on("click","#head-user-pic",function()	{
+		$("#head-pro-section").fadeIn(100);
+	});
 	$(document).on("click","#more-link",function()	{
-		$("#head-nav-menu").show();
+		$("#head-more-menu").fadeIn(100);
+	});
+	$(document).on("click","#not-btn",function()	{
+		$("#notification-section").fadeIn(100);
+	});  
+	$("#notify-mob").click(function()	{
+		$("#show-notify-section-mobile").toggle();
 	}); 
 });
 
@@ -52,3 +62,53 @@ function fetchQuestionsMobile(txt,slashes)	{
 		}
 	});
 }
+function updateNotify(notify_id)	{
+	var slash=document.getElementById("slash").value;
+	$.ajax({
+		type:"post",
+		url:slash+"notifications.php",
+		data:
+		{
+			"slash":slash,
+			"notify_typ":"UPDATE",
+			"notify_id":notify_id
+		},
+		success:function(res)	{
+			if(res==1)	{
+				if(notify_id != -1)		{
+					$("#notify-message-"+notify_id).removeClass("list-view-on");
+					var countEle = $("#show-notify-section .list-view-on").length;
+					$("#notify-read-count").html(countEle);
+				}
+				else	{
+					/*if ($("#show-notify-section .list-group-item").hasClass('list-view-on'))
+						$("#show-notify-section .list-group-item").removeClass("list-view-on");*/
+					$("#notify-read-count").html(0);
+				}
+			}
+		}
+	});
+}	
+function loadNotify()	{
+	var slash=document.getElementById("slash").value;
+	$.ajax({
+		type:"post",
+		url:slash+"notifications.php",
+		data:
+		{
+			"slash":slash,
+			"notify_typ":"DISPLAY"
+		},
+		success:function(result)	{
+			$("#show-notify-section").html(result);
+			$("#show-notify-section-mobile").html(result);
+			var countEle = $("#show-notify-section .list-view-on").length;
+			$("#notify-read-count").html(countEle);
+		}
+	});
+}
+function refreshNotify()	{
+	loadNotify();
+	setInterval(loadNotify,2000);
+}
+
