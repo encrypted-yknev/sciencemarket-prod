5e59c324d115e3d369c76fc80492198d4298ff8a
N Venkateshan <venkycse93@gmail.com>
2018-02-04 13:17:39 +0530

    New file for displaying users in inbox page

---
+++ forum/inbox_leftpane.php
@@ -0,0 +1,52 @@
+		<div id="side-nav">
+			<table border="0">
+				<tr>
+					<td>
+						<div id="nav-id">
+							<div class="side-bar"></div>
+							<div class="side-bar"></div>
+							<div class="side-bar"></div>
+						</div>
+					</td>
+					<td>
+						<div id="media-image"><img src="../img/logo.jpg" width="200" height="50"/></div>
+					</td>
+				</tr>
+			</table></br>
+			<div id="page-title"><span>Inbox</span></div></br>
+			<!--
+			<div class="row">
+				<div class="col-sm-3">
+					<div id="row-1">
+						<a href="../qstn.php" class="btn btn-info">Ask Questions</a>
+					</div>
+					
+				</div>
+				<div class="col-sm-6">
+					<div id="row-2">
+						<input type="text" class="form-control" id="srch-box-media" placeholder="Search questions" onkeypress="fetchQuestionsMobile(this.value,'../')" />
+						<div id="srch-result-mobile"></div> 
+					</div>
+				</div>
+			</div>
+			-->
+		</div>
+		<div id="options-menu">
+			<ul class="nav nav-pills nav-stacked">
+				<li><a href="../profile.php" ><span class="glyphicon glyphicon-user" ></span>&nbsp;My Profile</a></li>
+				<li><a href="../dashboard.php"><span class="glyphicon glyphicon-home" ></span>&nbsp;Dashboard</a></li>
+				<li><a href="" ><span class="glyphicon glyphicon-question-sign"></span>&nbsp;Q/A Forum</a></li>
+				<li><a href="../expert_connect.php" ><span class="glyphicon glyphicon-transfer" ></span>&nbsp;Expert Connect</a></li>
+				<li><a href="" ><span class="glyphicon glyphicon-refresh" ></span>&nbsp;Collaborate</a></li>
+				<li><a href="" ><span class="glyphicon glyphicon-gift" ></span>&nbsp;Favours</a></li>
+				<li><a href="../logout.php"><span class="glyphicon glyphicon-off"></span>&nbsp;Logout</a></li>
+			</ul>
+		</div>
+		</br>
+		<div class="row">
+			<!--<div class="col-sm-2" id="side-qstn-cat">
+			<div id="fixed-div-layout">
+				<li class="side-menu"><a href="">Inbox</a></li>
+				<li class="side-menu"><a href="../inbox.php?type=2" >Sent Messages</a></li>
+			</div>
+			</div>-->
