86072b0f5742f7e215becccbe09bdf6d0b5feac5
N Venkateshan <venkycse93@gmail.com>
2018-02-04 14:08:34 +0530

    Modified the query and added extra feature

--- disp_ans_enter.php
+++ disp_ans_enter.php
@@ -1,19 +1,42 @@
 <?php 
 		try	{
-			$sql_show_some_ans = "select distinct  a.ans_id,
-												   a.ans_desc,
-												   a.up_votes,
-												   a.down_votes,
-												   a.posted_by,
-												   a.created_ts 
-										   from answers a
-										   inner join questions b 
-										   on a.qstn_id=b.qstn_id
-										   left outer join comments c
-										   on a.ans_id=c.ans_id
-										   where b.qstn_id=".$qid."
-										   order by a.created_ts desc,c.created_ts desc
-										   limit 3";
+			$sql_show_some_ans = "select z.ans_id,
+									   z.ans_desc,
+									   z.up_votes,
+									   z.down_votes,
+									   z.posted_by,
+									   z.created_ts,
+									   z.cmnt_creat_ts
+								from
+								(select a.ans_id,
+									   a.ans_desc,
+									   a.up_votes,
+									   a.down_votes,
+									   a.posted_by,
+									   a.created_ts,
+									   a.cmnt_creat_ts,
+									   @r:=case when a.ans_id=@a then @r+1
+											   else 1
+											   end as rnum,
+									   @a:=a.ans_id as row_ans_id
+								from 
+								(select @r:=0,@a:=0) t1,
+								(select a.ans_id,
+									   a.ans_desc,
+									   a.up_votes,
+									   a.down_votes,
+									   a.posted_by,
+									   a.created_ts,
+									   c.created_ts as cmnt_creat_ts
+								from answers a
+								inner join questions b 
+								on a.qstn_id=b.qstn_id
+								left outer join comments c
+								on a.ans_id=c.ans_id
+								where b.qstn_id=".$qid.") a) z
+								where z.rnum = 1
+								order by z.created_ts desc,z.cmnt_creat_ts desc
+								limit 3";
 			$sql_count_ans = "select count(1) from answers where qstn_id = ".$qid;
 			
 			$stmt_show_some_ans=$conn->prepare($sql_show_some_ans);
@@ -34,14 +57,30 @@
 					$stmt_get_user_pic = $conn->prepare($sql_get_user_pic);
 					$stmt_get_user_pic->execute();
 					$row_pic = $stmt_get_user_pic->fetch();
-					$ans_user_pic = $slashes.$row_pic['pro_img_url'];
+					$ans_user_pic = $row_pic['pro_img_url'];
 					?>
 					<div class="ans-front-hidden-sec" id="ans-front-sec-<?php echo $ansid; ?>">
 						<div class="photo-ans-sec" style="background-image:url('<?php echo $ans_user_pic; ?>'); background-size:cover;"></div>
 							
 						<div class="auth-text-section">
-							<?php echo '<strong>'.$ans_user.'</strong> - <span class="time-sec">'.get_user_date(convert_utc_to_local($ans_ts)).'</span>'; ?></br>
+							<?php 
+							echo "<span id='ans-posted-".$ansid."' onmouseleave='showUserCard(event,1,".$ansid.",\"fa\")' onmouseenter='showUserCard(event,0,".$ansid.",\"fa\")'><a href='".$slashes."profile.php?user=".$ans_user."'>".$ans_user."</a></span> . ".
+							get_user_date(convert_utc_to_local($ans_ts));
+							?>
+						</br>
 						</div></br>
+						<?php 
+							$user_id_fetch=$ans_user;
+							
+							include "fetch_user_dtls.php";
+							
+							$post_type="fa";
+							$user_card=$ans_user;
+							$up_vote=$up_user_votes;
+							$down_vote=$down_user_votes;
+							$id=$ansid;
+							include "user_card.php"; 
+						?>
 						<div class="ans-text-section"><?php echo $ans."</br>"; ?></div></br>
 						<?php 
 							$sql_check_up_vote = "select count(1) as vote_count from user_posts_votes where user_id='".$_SESSION['user']."' and post_type='A' and vote_type=0 and post_id=".$ansid;
@@ -96,59 +135,70 @@
 						</div>
 						<div class="comment-section" id="comment-front-<?php echo $ansid; ?>">
 						</br>
-						<input type="text" class="form-control comment-inp" id="comment-front-ans-<?php echo $ansid; ?>" placeholder="Leave comment" 
-						onkeypress=""/>
-						
-						</br>
-						<button type="button" class="btn btn-primary" style="padding: 1px 2px;" 
-						onclick="addComment(2,<?php echo "'".$slashes."',".$ansid.",'".$ans_user."',".$qid.",'".$posted_by."'"; ?>)">Comment</button></br></br>
-						
 						<div class="comments-list" id="comment-area-front-<?php echo $ansid; ?>">
 						<?php
 							try	{
-											$comment_array=array();
-											$comment_id_str="";
-											$sql_fetch_comment_ids="select comment_id from comments where ans_id=".$ansid." order by created_ts desc";
-											$stmt_fetch_comment_ids=$conn->prepare($sql_fetch_comment_ids);
-											$stmt_fetch_comment_ids->execute();
-											if($stmt_fetch_comment_ids->rowCount() > 0)	{
-												while($row = $stmt_fetch_comment_ids->fetch())	{
-													$cmt_id=$row['comment_id'];
-													array_push($comment_array,$cmt_id);
-												}
-												$comment_id_str=implode("|",$comment_array);
-											}
-											
-											$sql_fetch_comment="select comment_id,comment_desc,posted_by,created_ts from comments where 	ans_id=".$ansid." order by created_ts desc limit 5";
-											$stmt_fetch_comment=$conn->prepare($sql_fetch_comment);
-											$stmt_fetch_comment->execute();
-											
-											if($stmt_fetch_comment->rowCount() > 0)	{
-												while($row_cmnt = $stmt_fetch_comment->fetch())	{
-													$comment_id=$row_cmnt['comment_id'];
-													$comment=$row_cmnt['comment_desc'];
-													$cmnt_posted_by=$row_cmnt['posted_by'];
-													$created_ts = $row_cmnt['created_ts'];
-													echo '<div class="user-comment-sec" id="comment-list-front-'.$comment_id.'">'.$comment.' - <strong>'.$cmnt_posted_by.'</strong>&nbsp;&nbsp;<span class="time-sec">'.get_user_date(convert_utc_to_local($created_ts)).'</span></div>';
-												}
-											}
-											else	{
-												echo "No comments in this answer yet";
-											}
-										}
-										catch(PDOException $e)	{
-											echo "Internal server error";
-										}
-									?>
-									</div></br>
-									<?php
-									$comment_count = $stmt_fetch_comment_ids->rowCount();
-									if($comment_count > 5)
-										echo "<span id='comment-load-front-text-".$ansid."' href='javascript:void(0)' onclick='loadMoreComments(2,\"".$slashes."\",".$ansid.")' class='show-comment-text'>View more comments</span>";
-									?>
-									<input id="cid-front-section-<?php echo $ansid; ?>" type="hidden" value="<?php echo $comment_id_str; ?>"/>
-					</div></br>									
+								$comment_array=array();
+								$comment_id_str="";
+								$sql_fetch_comment_ids="select comment_id from comments where ans_id=".$ansid." order by created_ts desc";
+								$stmt_fetch_comment_ids=$conn->prepare($sql_fetch_comment_ids);
+								$stmt_fetch_comment_ids->execute();
+								if($stmt_fetch_comment_ids->rowCount() > 0)	{
+									while($row = $stmt_fetch_comment_ids->fetch())	{
+										$cmt_id=$row['comment_id'];
+										array_push($comment_array,$cmt_id);
+									}
+									$comment_id_str=implode("|",$comment_array);
+								}
+								
+								$sql_fetch_comment="select comment_id,comment_desc,posted_by,created_ts from comments where 	ans_id=".$ansid." order by created_ts desc limit 5";
+								$stmt_fetch_comment=$conn->prepare($sql_fetch_comment);
+								$stmt_fetch_comment->execute();
+								
+								if($stmt_fetch_comment->rowCount() > 0)	{
+									echo "<div class='cmnt-section' id='cmnt-list-".$ansid."'>";
+									while($row_cmnt = $stmt_fetch_comment->fetch())	{
+										$comment_id=$row_cmnt['comment_id'];
+										$comment=$row_cmnt['comment_desc'];
+										$cmnt_posted_by=$row_cmnt['posted_by'];
+										$created_ts = $row_cmnt['created_ts'];
+										
+										echo "<div class='user-comment-sec' id='comment-list-front-".$comment_id."'>".$comment." - <strong><span id='cmn-posted-".$comment_id."' onmouseleave='showUserCard(1,".$comment_id.",\"fc\")' onmouseenter='showUserCard(event,0,".$comment_id.",\"fc\")'><a href='".$slashes."profile.php?user=".$cmnt_posted_by."'>".$cmnt_posted_by."</a></span></strong>&nbsp;&nbsp;<span class='time-sec'>".get_user_date(convert_utc_to_local($created_ts))."</span></div>";
+										
+										$user_id_fetch=$cmnt_posted_by;
+										
+										include "fetch_user_dtls.php";
+										
+										$post_type="fc";
+										$id=$comment_id;
+										$user_card=$cmnt_posted_by;
+										$up_vote=$up_user_votes;
+										$down_vote=$down_user_votes;
+										include "user_card.php"; 
+									}
+									echo "</div>";
+								}
+								else	{
+									echo "No comments in this answer yet";
+								}
+							}
+							catch(PDOException $e)	{
+								echo "Internal server error";
+							}
+					?>
+					<input type="text" class="form-control comment-inp" id="comment-front-ans-<?php echo $ansid; ?>" placeholder="Leave comment" onkeypress="addComment(event,2,<?php echo "'".$slashes."',".$ansid.",'".$ans_user."',".$qid.",'".$posted_by."'"; ?>)"/>
+					</br>
 					</div>
+					</br>
+					<?php
+					$comment_count = $stmt_fetch_comment_ids->rowCount();
+					if($comment_count > 5)
+						echo "<span id='comment-load-front-text-".$ansid."' href='javascript:void(0)' onclick='loadMoreComments(2,\"".$slashes."\",".$ansid.")' class='show-comment-text'>View more comments</span>";
+					?>
+					<input id="cid-front-section-<?php echo $ansid; ?>" type="hidden" value="<?php echo $comment_id_str; ?>"/>
+					</div></br>	
+					</div>								
+					
 					<?php
 				}
 			}
