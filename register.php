078fbd9319e21fc7c44b57b26da23669c987890a
N Venkateshan <venkycse93@gmail.com>
2018-02-04 14:53:46 +0530

    Modified the code to support few changes
    1. Removed the header stylesheet
    2. Added form tips while the user focuses on the textbox input
    3. On successful registration, redirect user to add interests rather than going directly into dashboard page.

--- register.php
+++ register.php
@@ -5,7 +5,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Science Market : Get started. Register here.</title>
 <link rel="stylesheet" type="text/css" href="styles/register.css">
-<link rel="stylesheet" type="text/css" href="styles/header.css">
 <link rel="stylesheet" type="text/css" href="styles/footer.css">
 <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -33,18 +32,18 @@ if($_SERVER["REQUEST_METHOD"]=="POST")	{
 	#If everything is fine - Add the user.
 		try	{
 			$sql="insert into users
-			(user_id,disp_name,encrypt_pwd,email_addr,pro_img_url,status)
+			(user_id,disp_name,encrypt_pwd,email_addr,status)
 			values
-			('".$userid."','".$name."','".$hashed_pwd."','".$mail."','uploads/male.jpg','A')";
+			('".$userid."','".$name."','".$hashed_pwd."','".$mail."','A')";
 			
 			$conn->exec($sql);
 			$message = "Registration successful";
 			session_start();
 			include "session.php";	#Starting user session
-			header("location:dashboard.php");		#On successful registration, redirect to dashboard
+			header("location:user_preference.php");		#On successful registration, redirect to dashboard
 		}
 		catch(PDOException $e)	{
-			$message = "Registration failed ";
+			$message = "Registration failed ".$e->getMessage();
 		}
 	}
 	else
@@ -58,8 +57,9 @@ if($_SERVER["REQUEST_METHOD"]=="POST")	{
 <div class="container">
 	<div id="reg-container">
 			<div class="row">
-				<div class="col-sm-4">
-					<img src="img/logo.jpg" width="200" height="50"/>
+				<div id="logo-section" class="col-sm-4">
+					<img src="img/logo4.svg" width="55" height="55"/>
+					<img src="img/logo.svg" width="140" height="60"/>
 				</div>
 				<div class="col-sm-8">
 					<span id="page-caption"><em>An online portal to discuss stuffs among peers, connect with experts and collaborate with people. Register now to get started</em></span>
@@ -71,8 +71,9 @@ if($_SERVER["REQUEST_METHOD"]=="POST")	{
 				<div class="form-group">
 					<label class="control-label col-sm-4 form-label" for="name">Enter full name</label>
 					<div class="col-sm-4">
-						<input class="form-control" id="name" type="text" name="name" value="<?php echo $name; ?>" 
-							onfocus="showTip(1)" onfocusout="validateData(this.value,'name-error')" />
+						<input class="form-control" id="name" type="text" name="name" onfocus="$('#msg-1').show();" onblur="$('#msg-1').hide();" value="<?php echo $name; ?>" 
+							onfocusout="validateData(this.value,'name-error')" /> 
+						<div class="msg-info" id="msg-1">Enter valid name. Should contain only spaces and alphabets</div>
 					</div>
 					<div class="col-sm-4 message-section">
 						<span id="name-error" class="col-3-data"></span>
@@ -81,8 +82,9 @@ if($_SERVER["REQUEST_METHOD"]=="POST")	{
 				<div class="form-group">
 					<label class="control-label col-sm-4 form-label" for="user">Choose UserID</label>
 					<div class="col-sm-4">
-						<input class="form-control" type="text" id="user" name="user" value="<?php echo $userid; ?>" 
-								onfocus="showTip(2)" onfocusout="validateUser(this.value)" />
+						<input class="form-control" type="text" id="user" name="user" onfocus="$('#msg-2').show();" onblur="$('#msg-2').hide();" value="<?php echo $userid; ?>" 
+							 onfocusout="validateUser(this.value)" />
+						<div class="msg-info" id="msg-2">Username must be unique</div>
 					</div>
 					<div class="col-sm-4 message-section">
 						<span id="user-error" class="col-3-data"></span>
@@ -91,8 +93,9 @@ if($_SERVER["REQUEST_METHOD"]=="POST")	{
 				<div class="form-group">
 					<label class="control-label col-sm-4 form-label" for="pwd">Choose password</label>
 					<div class="col-sm-4">
-						<input class="form-control" type="password" id="pwd" name="pwd" 
-								onfocus="showTip(3)" onfocusout="validatePassFld(this.value,'pwd-error')" />
+						<input class="form-control" type="password" id="pwd" name="pwd" onfocus="$('#msg-3').show();" onblur="$('#msg-3').hide();"
+							 onfocusout="validatePassFld(this.value,'pwd-error')" />
+						<div class="msg-info" id="msg-3">Password should contain minimum of 8 characters</div>
 					</div>
 					<div class="col-sm-4 message-section">
 						<span id="pwd-error" class="col-3-data"></span>
@@ -101,8 +104,9 @@ if($_SERVER["REQUEST_METHOD"]=="POST")	{
 				<div class="form-group">
 					<label class="control-label col-sm-4 form-label" for="repwd">Re-enter password</label>
 					<div class="col-sm-4">
-						<input class="form-control" type="password" id="repwd" 
-								onfocus="showTip(4)" onfocusout="validateRePassFld(document.getElementById('pwd').value,this.value,'repass-error')" />
+						<input class="form-control" type="password" id="repwd" onfocus="$('#msg-4').show();" onblur="$('#msg-4').hide();"
+								onfocusout="validateRePassFld(document.getElementById('pwd').value,this.value,'repass-error')" />
+						<div class="msg-info" id="msg-4">Re-enter same password as above</div>
 					</div>
 					<div class="col-sm-4 message-section">
 						<span id="repass-error" class="col-3-data"></span>
@@ -111,15 +115,17 @@ if($_SERVER["REQUEST_METHOD"]=="POST")	{
 				<div class="form-group">
 					<label class="control-label col-sm-4 form-label" for="mail">Email</label>
 					<div class="col-sm-4">
-						<input class="form-control" type="text" id="mail" name="mail" value="<?php echo $mail; ?>" onfocus="showTip(5)" />
+						<input class="form-control" type="text" id="mail" name="mail" onfocus="$('#msg-5').show();" onblur="$('#msg-5').hide();" value="<?php echo $mail; ?>" />
+						<div class="msg-info" id="msg-5">Enter valid email of format abc@xyz.pqr</div>
 					</div>
 					<div class="col-sm-4 message-section">
 						<span id="user-error" class="col-3-data"></span>
 					</div>
 				</div></br>
 				<div id="button-section">
-					<input id="reg-button" class="btn btn-default" type="submit" value="Register" />
-					<input id="res-button" class="btn btn-default" type="reset" value="Reset" />
+					<input id="reg-button" class="btn btn-default nav-button" type="submit" value="Register" />
+					<input id="res-button" class="btn btn-default nav-button" type="reset" value="Reset" />
+					<a href="index.php" id="home-button" class="btn btn-primary nav-button">Home</a>
 				</div>
 			</form>
 	</div>	
