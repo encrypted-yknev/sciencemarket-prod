bda6a764e10c9b731432cac67b753fb1eb09a349
N Venkateshan <venkycse93@gmail.com>
2018-02-04 14:18:02 +0530

    Modified bugs and added extra features

--- fetch_ans.php
+++ fetch_ans.php
@@ -1,14 +1,24 @@
 <?php 
 session_start();
 
+if(isset($_SESSION['logged_in']) and $_SESSION['logged_in'])
+	$logged_in=1;
+else
+	$logged_in=0;
+
 include "connectDb.php";
 include "forum/functions/get_time.php";
 include "forum/functions/get_time_offset.php";
 function convert_utc_to_local($utc_timestamp)	{
-	$date_utc=new DateTime($utc_timestamp,new DateTimeZone('UTC'));
-	$date_utc->setTimeZone(new DateTimeZone($_COOKIE['user_tz']));
-	$date_final = $date_utc->format('Y-m-d H:i:s');
-	return $date_final;
+	try	{
+		$date_utc=new DateTime($utc_timestamp,new DateTimeZone('UTC'));
+		$date_utc->setTimeZone(new DateTimeZone($_COOKIE['user_tz']));
+		$date_final = $date_utc->format('Y-m-d H:i:s');
+		return $date_final;
+	}
+	catch(Exception $e)	{
+		
+	}
 }
 $ans_list="";
 if(isset($_REQUEST['qid']))	{
@@ -76,7 +86,7 @@ while($start_ans <= $end_ans)	{
 			$stmt_get_user_pic = $conn->prepare($sql_get_user_pic);
 			$stmt_get_user_pic->execute();
 			$row_pic = $stmt_get_user_pic->fetch();
-			$ans_user_pic = $slashes.$row_pic['pro_img_url'];
+			$ans_user_pic = $row_pic['pro_img_url'];
 			
 			if($source_flag == 'r')	{
 				$id_upvote_val = "upvote-value-ans-".$ansid;
@@ -95,6 +105,9 @@ while($start_ans <= $end_ans)	{
 				$comment_text1 = "comment-load-recent-text-".$ansid;
 				$comment_text2 = "comment-display-recent-text-".$ansid;
 				$input_comment_text = "cid-recent-section-".$ansid;
+				$comment_list = "cmnt-list-recent-".$ansid;
+				$card_ans="ra";
+				$card_cmnt="rc";
 				$token = 0;
 			}
 			else	{
@@ -114,17 +127,40 @@ while($start_ans <= $end_ans)	{
 				$comment_text1 = "comment-load-top-text-".$ansid;
 				$comment_text2 = "comment-display-top-text-".$ansid;
 				$input_comment_text = "cid-top-section-".$ansid;
+				$comment_list = "cmnt-list-top-".$ansid;
+				$card_ans="ta";
+				$card_cmnt="tc";
 				$token = 1;
 			}
 					?>
 					<div class="<?php echo $id_main_cls; ?>" id="<?php echo $id_main_sec; ?>">
-						<div class="photo-ans-sec" style="background-image:url('<?php echo $ans_user_pic; ?>'); background-size:cover;"></div>
+						<div class="photo-ans-sec" 
+						onmouseleave='showUserCard(event,1,<?php echo $ansid; ?>,"<?php echo $card_ans; ?>")' 
+						onmouseenter='showUserCard(event,0,<?php echo $ansid; ?>,"<?php echo $card_ans; ?>")'
+						style="background-image:url('<?php echo $ans_user_pic; ?>'); background-size:cover;"></div>
 							
 						<div class="auth-text-section">
-							<?php echo '<strong>'.$ans_user.'</strong> - <span class="time-sec">'.get_user_date(convert_utc_to_local($ans_ts)).'</span>'; ?></br>
+							<?php 
+							echo "<span id='ans-posted-".$ansid."' onmouseleave='showUserCard(event,1,".$ansid.",\"".$card_ans."\")' onmouseenter='showUserCard(event,0,".$ansid.",\"".$card_ans."\")'><a href='".$slashes."profile.php?user=".$ans_user."'>".$ans_user."</a></span> . ".
+							get_user_date(convert_utc_to_local($ans_ts));
+							?>
+						</br>
 						</div></br>
+						<?php 
+							$user_id_fetch=$ans_user;
+							
+							include "fetch_user_dtls.php";
+							
+							$post_type=$card_ans;
+							$user_card=$ans_user;
+							$up_vote=$up_user_votes;
+							$down_vote=$down_user_votes;
+							$id=$ansid;
+							include "user_card.php"; 
+						?>
 						<div class="ans-text-section"><?php echo $ans."</br>"; ?></div></br>
 						<?php 
+						if($logged_in == 1)	{
 							$sql_check_up_vote = "select count(1) as vote_count from user_posts_votes where user_id='".$_SESSION['user']."' and post_type='A' and vote_type=0 and post_id=".$ansid;
 							$sql_check_down_vote = "select count(1) as vote_count from user_posts_votes where user_id='".$_SESSION['user']."' 
 												and post_type='A' and vote_type=1 and post_id=".$ansid;
@@ -136,7 +172,11 @@ while($start_ans <= $end_ans)	{
 							$stmt_check_down_vote->execute();
 							$sql_row_1 = $stmt_check_down_vote->fetch();
 							$count_row_1 = $sql_row_1['vote_count'];
-								
+						}
+						else	{
+							$count_row_0 = 0;
+							$count_row_1 = 0;
+						}
 							?>
 						<input type="hidden" id="<?php echo $id_upvote_val; ?>" value="<?php echo $count_row_0; ?>" />
 						<input type="hidden" id="<?php echo $id_downvote_val; ?>" value="<?php echo $count_row_1; ?>" />
@@ -144,13 +184,20 @@ while($start_ans <= $end_ans)	{
 						<div class="voting-links">
 							<span class="vote-sec">
 						<?php 
+							if($logged_in == 1)	{
 									if($ans_user != $_SESSION['user'])	{
 								?>
 							<span href="javscript:void(0)" class="vote-link-area" style="cursor:pointer;"
 								onclick="increaseAnsCount('<?php echo $ansid."','".$ans_user."'";?>,0,'<?php echo $slashes; ?>', document.getElementById('<?php echo $id_upvote_val; ?>').value,<?php echo ($source_flag == 'r'?0:1); ?>)">
 								<span id="<?php echo $id_glyp_up_val; ?>" class="glyphicon glyphicon-thumbs-up <?php echo ($count_row_0 > 0)?"glyph-ans-upvoted":"";  ?>"></span>
 							<span id="<?php echo $id_upvote_ans_val; ?>" class="vote-count-area"><?php echo $upvotes; ?></span></span>
-							<?php } 
+							<?php }
+								else	{
+									?>
+								<span class="glyphicon glyphicon-thumbs-up"></span>
+								<span id="<?php echo $id_upvote_ans_val; ?>" class="vote-count-area"><?php echo $upvotes; ?></span>
+								<?php } 
+							}
 										else	{
 									?>
 								<span class="glyphicon glyphicon-thumbs-up"></span>
@@ -159,13 +206,20 @@ while($start_ans <= $end_ans)	{
 						</span>
 						<span class="vote-sec">
 						<?php 
+						if($logged_in == 1)	{
 									if($ans_user != $_SESSION['user'])	{
 								?>
 							<span href="javscript:void(0)" class="vote-link-area" style="cursor:pointer;"
 								onclick="increaseAnsCount('<?php echo $ansid."','".$ans_user."'";?>,1,'<?php echo $slashes; ?>',document.getElementById('<?php echo $id_downvote_val; ?>').value,<?php echo ($source_flag == 'r'?0:1); ?>)">
 								<span id="<?php echo $id_glyp_down_val; ?>"  class="glyphicon glyphicon-thumbs-down <?php echo ($count_row_1 > 0)?"glyph-ans-downvoted":"";  ?>"></span>
 							<span id="<?php echo $id_downvote_ans_val; ?>" class="vote-count-area"><?php echo $downvotes; ?></span></span>
-						<?php } 
+						<?php }
+							else	{
+								?>
+							<span class="glyphicon glyphicon-thumbs-down"></span>
+							<span id="<?php echo $id_downvote_ans_val; ?>" class="vote-count-area"><?php echo $downvotes; ?></span>
+							<?php } 
+						}
 									else	{
 								?>
 							<span class="glyphicon glyphicon-thumbs-down"></span>
@@ -177,19 +231,12 @@ while($start_ans <= $end_ans)	{
 						
 					<div class="comment-section" id="<?php echo $comment_section;?>">
 						</br>
-						<input type="text" class="form-control comment-inp" id="<?php echo $comment_ans; ?>" placeholder="Leave comment" 
-						onkeypress=""/>
-						
-						</br>
-						<button type="button" class="btn btn-primary" style="padding: 2px 2px; font-size:12px;" 
-						onclick="addComment(<?php echo $token.",'".$slashes."',".$ansid.",'".$ans_user."',".$qid.",'".$postedBy."'"; ?>)">Comment</button></br></br>
-						
 						<div class="comments-list" id="<?php echo $comment_area; ?>">
 						<?php
 							try	{
 								$comment_array=array();
 								$comment_id_str="";
-								$sql_fetch_comment_ids="select comment_id from comments where ans_id=".$ansid." order by created_ts desc";
+								$sql_fetch_comment_ids="select comment_id from comments where ans_id=".$ansid." order by created_ts asc";
 								$stmt_fetch_comment_ids=$conn->prepare($sql_fetch_comment_ids);
 								$stmt_fetch_comment_ids->execute();
 								if($stmt_fetch_comment_ids->rowCount() > 0)	{
@@ -204,13 +251,32 @@ while($start_ans <= $end_ans)	{
 								$stmt_fetch_comment=$conn->prepare($sql_fetch_comment);
 								$stmt_fetch_comment->execute();
 								if($stmt_fetch_comment->rowCount() > 0)	{
+									echo "<div class='cmnt-section' id='".$comment_list."'>";
 									while($row_cmnt = $stmt_fetch_comment->fetch())	{
 										$comment_id=$row_cmnt['comment_id'];
 										$comment=$row_cmnt['comment_desc'];
-										$posted_by=$row_cmnt['posted_by'];
+										$cmnt_posted_by=$row_cmnt['posted_by'];
 										$created_ts = $row_cmnt['created_ts'];
-										echo '<div class="user-comment-sec" id="'.$comment_id_name.$comment_id.'">'.$comment.' - <strong>'.$posted_by.'</strong>&nbsp;&nbsp;<span class="time-sec">'.get_user_date(convert_utc_to_local($created_ts)).'</span></div>';
+										
+										echo "<div class='user-comment-sec' id='".$comment_id_name.$comment_id."'>".$comment." - <strong>
+										<span id='cmn-posted-".$comment_id."' 
+										onmouseleave='showUserCard(event,1,".$comment_id.",\"".$card_cmnt."\")' 
+										onmouseenter='showUserCard(event,0,".$comment_id.",\"".$card_cmnt."\")'>
+										<a href='".$slashes."profile.php?user=".$cmnt_posted_by."'>".$cmnt_posted_by."</a></span></strong>&nbsp;&nbsp;
+										<span class='time-sec'>".get_user_date(convert_utc_to_local($created_ts))."</span></div>";
+										
+										$user_id_fetch=$cmnt_posted_by;
+													
+										include "fetch_user_dtls.php";
+										
+										$post_type=$card_cmnt; 
+										$id=$comment_id;
+										$user_card=$cmnt_posted_by;
+										$up_vote=$up_user_votes;
+										$down_vote=$down_user_votes;
+										include "user_card.php"; 
 									}
+									echo "</div>";
 								}
 								else	{
 									echo "No comments in this answer yet";
@@ -220,6 +286,10 @@ while($start_ans <= $end_ans)	{
 								echo "Internal server error";
 							}
 						?>
+						<input type="text" class="form-control comment-inp" id="<?php echo $comment_ans; ?>" placeholder="Leave comment" 
+						onkeypress="addComment(event,<?php echo $token.",'".$slashes."',".$ansid.",'".$ans_user."',".$qid.",'".$postedBy."'"; ?>)" onfocus="showAlert(0,<?php echo $logged_in; ?>)"/>
+						
+						</br>
 						</div></br>
 						<?php
 						$comment_count = $stmt_fetch_comment_ids->rowCount();
