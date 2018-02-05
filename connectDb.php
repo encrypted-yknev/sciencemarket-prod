296fff57418d65ac249341b029684988a6873059
N Venkateshan <venkycse93@gmail.com>
2018-02-06 00:56:32 +0530

    Removed the commented portion

--- connectDb.php
+++ connectDb.php
@@ -9,7 +9,6 @@ $servername="localhost";
 try	{
 	$conn=new PDO("mysql:host=$servername;dbname=testdb",$username,$password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	#echo "Connection Succeeded";
 }
 
 catch(PDOException $e)	{
